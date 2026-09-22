b3aff8e44e0ded7b15b002a0d13a8552e247cf60
Nobody <149746398+nobodyhere4@users.noreply.github.com>
2026-09-22 16:17:54 -0500

    Non yams teleop classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008HM2027.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Team4008HM2027 {
+    public DcMotor DriveRightBack = null;
+    public DcMotor DriveLeftBack = null;
+    public DcMotor DriveLeftFront = null;
+    public DcMotor DriveRightFront = null;
+
+    public double num = 0;
+
+
+
+
+    HardwareMap hwMap = null;
+    public void Map(HardwareMap hardwareMap)
+    {
+        hwMap = hardwareMap;
+        num = 67;
+        DriveLeftFront = hwMap.get(DcMotor.class,"DriveLeftFront");
+        DriveRightFront = hwMap.get(DcMotor.class,"DriveRightFront");
+        DriveLeftBack = hwMap.get(DcMotor.class,"DriveLeftBack");
+        DriveRightBack = hwMap.get(DcMotor.class,"DriveRightBack");
+        //   Lights = hwMap.get(RevBlinkinLedDriver.class,"Lights");
+
+        DriveLeftFront.setDirection(DcMotor.Direction.FORWARD);
+        DriveLeftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        /* If you are using encoders, set to RUN_USING_ENCODER.
+         If you put RUN_USING_ENCODERS and there is no encoder value getting
+         recieved then the motor power is set to 1.0 even if you put a different
+         value */
+        DriveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveRightFront.setDirection(DcMotor.Direction.REVERSE);
+        DriveRightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        DriveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveLeftBack.setDirection(DcMotor.Direction.FORWARD);
+        DriveLeftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        DriveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveRightBack.setDirection(DcMotor.Direction.REVERSE);
+        DriveRightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveRightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        DriveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+}
